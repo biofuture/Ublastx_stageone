51107c376fe1bba58e7d4289e7e8cbc57b05bd48
lg209ws3 <lg209ws3@147.8.134.246>
2017-04-26 10:55:42 +0800

    modify split

--- split_big_files.pl
+++ split_big_files.pl
@@ -4,9 +4,9 @@ use strict;
 ##This script help to split the big fastq files into smaller one so that 32-bit usearch could process the fastq files
 ##The splitted fastq files are named by Name_index_1.fq and Name_index_2.fq
 
-die "perl $0 <IN_1.fq> <IN_2.fq> <Seq_Numb> <Outdir>\n" unless (@ARGV == 3);
+die "perl $0 <IN_1.fq> <IN_2.fq> <Seq_Numb> <Outdir>\n" unless (@ARGV == 4);
 ##The default Seq_Numb for splitted fastq is 1000000
-my $ARGV[2] =|| 10000000;
+$ARGV[2] ||= 1000000;
 
 my $index  = 1;
 
@@ -14,13 +14,21 @@ die "$!\n" unless open(I, "$ARGV[0]");
 die "$!\n" unless open(II, "$ARGV[1]");
 
 my $o1 = $ARGV[0];
-my $pr1 = s/\_1\.fq//;
-$o1 =~ s/\_1\.fq/\_$index\_1\.fq/;
+$o1 = (split(/\//, $o1))[-1];
+$o1 =~ s/\_1\.fq//;
+my $pr1 = $o1;
+$o1 = "$pr1-part-$index\_1.fq";
 die "$!" if($o1 eq $ARGV[0]);
 my $o2 = $ARGV[1];
-$o2 =~ s/\_1\.fq/\_$index\_1\.fq/;
-my $pr2 = s/\_2\.fq//;
-die "$!" if($o2 eq $ARGV[0]);
+$o2 = (split(/\//, $o2))[-1];
+$o2 =~ s/\_2\.fq//;
+my $pr2 = $o2;
+$o2 = "$pr2-part-$index\_2.fq";
+die "$!" if($o2 eq $ARGV[1]);
+
+unless( -d "$ARGV[3]"){
+	`mkdir $ARGV[3]`;
+}
 
 $o1 = "$ARGV[3]/$o1";
 $o2 = "$ARGV[3]/$o2";
@@ -46,10 +54,18 @@ while(my $f1 = <I>){
 		close T; close TT;
 
 		##open new output files
-		my $on1 = "$ARGV[3]/$";
-		my $on2 = "$ARGV[3]/";
+		$o1 =  "$pr1-part-$index\_1.fq";
+		$o2 = "$pr2-part-$index\_2.fq";
+		my $on1 = "$ARGV[3]/$o1";
+		my $on2 = "$ARGV[3]/$o2";
 		die "$!\n" unless open(T, ">$on1");
 		die "$!\n" unless open(TT, ">$on2");
+		
+                print T "$f1$f2$f3$f4";
+                print TT "$q1$q2$q3$q4";
+                
+	
+		$num++;
 		
 
 	}else{
